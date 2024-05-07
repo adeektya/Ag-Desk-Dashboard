26301bfc5f962731c6c04cbc611c069ecb1c9c1b
leenuj6 <135097295+leenuj6@users.noreply.github.com>
2024-05-07 23:59:41 +1000

    Update TaskCardProps.ts to show status in taskcard

--- src/types/TaskCardProps.ts
+++ src/types/TaskCardProps.ts
@@ -2,6 +2,7 @@ export interface TaskCardProps {
     title: string;
     tasks: Task[];
     taskIndex: number;
+    status : string;
     severity: 'high' | 'medium' | 'low';
     toggleTaskCompletion: (taskId: string) => void; 
     description?: string;
@@ -11,6 +12,7 @@ export interface TaskCardProps {
   export interface Task {
     id: string;
     title: string;
+    status : string;
     completed: boolean;
     subtasks: string[];
     severity: 'high' | 'medium' | 'low';
