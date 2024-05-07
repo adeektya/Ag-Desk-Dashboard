067d1b086261117f1a9af8ca52d855b5c3eb5a65
leenuj6 <135097295+leenuj6@users.noreply.github.com>
2024-05-07 22:50:45 +1000

    Update api.ts
    update api end point

--- src/pages/EmployeePage/api.ts
+++ src/pages/EmployeePage/api.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'http://127.0.0.1:8000/employee/employees/';
+export const API_URL = 'http://127.0.0.1:8000/employee/';
 
 export const fetchEmployees = async () => {
   try {
