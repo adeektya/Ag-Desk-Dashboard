05f1f4181fdb263f5fb4b80d424462e04a00a284
miltonso <140776668+miltonso@users.noreply.github.com>
2024-05-15 03:38:18 +1000

    Update urls.py

--- ag_desk/vehicle_management/urls.py
+++ ag_desk/vehicle_management/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import vehicle_list, vehicle_detail
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import VehicleItemList, VehicleDetail
 
 urlpatterns = [
-    path('vehicles/', vehicle_list, name='vehicle-list'),  # URL pattern for listing vehicles
-    path('vehicles/<int:pk>/', vehicle_detail, name='vehicle-detail'),  # URL pattern for retrieving, updating, or deleting a specific vehicle
+    path('', VehicleItemList.as_view(), name='vehicle-list'),  # URL pattern for listing vehicles
+    path('<int:id>/', VehicleDetail.as_view(), name='vehicle-detail'),  # URL pattern for retrieving, updating, or deleting a specific vehicle
 ]
+
+if settings.DEBUG:  # Only serve media files through Django in development
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
