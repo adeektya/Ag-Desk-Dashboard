7f8c3ff53b7037a50e09519c1f0327ccb2aafaa5
leenuj6 <leenuj6@gmail.com>
2024-05-27 22:38:31 +1000

    bulk api handling in section management

--- ag_desk/section_management/urls.py
+++ ag_desk/section_management/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SectionItemList, SectionDetail
+from .views import SectionItemList, SectionDetail,BulkSectionItemView
 
 urlpatterns = [
     path('', SectionItemList.as_view(), name='section-list'),
     path('<int:id>/', SectionDetail.as_view(), name='section-detail'),
+    path('bulk/', BulkSectionItemView.as_view(), name='section-bulk'),
 ]
