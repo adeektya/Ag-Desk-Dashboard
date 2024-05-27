c76dc4ac49ac65c0abc5cb1b705387f57a782a6e
leenuj6 <leenuj6@gmail.com>
2024-05-27 23:00:34 +1000

    adding unique_constraint

--- ag_desk/employee_management/models.py
+++ ag_desk/employee_management/models.py
@@ -20,16 +20,15 @@ class Employee(models.Model):
     contactNumber = models.CharField(max_length=50)
     email = models.EmailField()
     farm = models.ForeignKey(Farm, on_delete=models.CASCADE, related_name='employees')
-    section = models.CharField(max_length=255)
+    section = models.CharField(max_length=255,null=True, blank=True)
     start_date = models.DateField()
-    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    salary = models.DecimalField(max_digits=10, decimal_places=2,null=True, blank=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="active")
     photo = models.ImageField(
         upload_to="employee_photos/", blank=True, null=True
     )  # Photo field added
     class Meta:
         # Add a unique constraint across email, contactNumber, and farm fields
-        unique_together = [ 'contactNumber', 'farm']
-        unique_together = ['email',  'farm']
+        unique_together = [('contactNumber', 'farm'), ('email', 'farm')]
     def __str__(self):
         return self.name
